fe3a21c9005663000b18b59f7575b37028a5ae8a
Artur-Aleksander Pärnoja <arthurp200148@gmail.com>
2021-12-20 17:15:33 +0200

    Add min and max temperature finding logic.

--- src/main/java/iot/weather/Controller/WeatherController.java
+++ src/main/java/iot/weather/Controller/WeatherController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -29,4 +30,17 @@ public class WeatherController {
         }
     }
 
+    @GetMapping("/temperature")
+    public ResponseEntity<String> getTemperature(@RequestParam(value = "id") Integer id) {
+        try {
+            String response = weatherService.getMinAndMaxTemperatureById(id);
+
+            if (response == null || response.trim().equals("")) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
